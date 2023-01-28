1b60b1c1b901cad9b34b94fe6dc50ca174385b58
appconnect-testing <122339450+appconnect-testing@users.noreply.github.com>
2023-01-28 10:52:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,14 @@ if __name__ == "__main__":
                     initial_sidebar_state='collapsed')
     st.title('🔨 Streamlit Template')
     st.line_chart([[1, 1, 1, 1], [1, 1, 1, 1]])
+    import os, ibm_db, ibm_db_dbi as dbi, pandas as pd
+    BergenKommune_DB2_Frankfurt_dsn = 'DATABASE={};HOSTNAME={};PORT={};PROTOCOL=TCPIP;UID={uid};PWD={pwd};SECURITY=SSL'.format(
+    'bludb',
+    'ba99a9e6-d59e-4883-8fc0-d6a8c9f7a08f.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud',
+    31321,
+    uid='sdy37069',
+    pwd="""T0UHbErcCaXx3uGF""")
+    BergenKommune_DB2_Frankfurt_connection = dbi.connect(BergenKommune_DB2_Frankfurt_dsn)
 
     st.markdown("""
         This app is only a template for a new Streamlit project. <br>
